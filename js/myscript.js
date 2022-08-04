f3521471f6173dda7767cc7d3f4f4411e6a20279
cwill83247 <breakthrumusic@hotmail.com>
2022-08-04 11:56:50 +0000

    commit message

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {            //we add this to amke sure the dom is loaded before runnign any jquery 
+    $("h2").addClass("underline"); //underlines h2 by referencing the class we created in our css 
+    $("ul").addClass("border");      //adds border referencing our CSS
+   
+
+})
